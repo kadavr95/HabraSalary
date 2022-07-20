56464b8145d60a6e447244a31f83bbcaf5bbff4a
Dmitry Yaskovich <kadavr95@outlook.com>
2022-07-20 22:57:40 +0300

    Add current salaries expected range based on data from all years

--- ProcessData.rb
+++ ProcessData.rb
@@ -1,7 +1,5 @@
 require 'csv'
 
-modifiers_sum = 0
-pct10_sum, pct25_sum, pct50_sum, pct75_sum, pct90_sum = 0, 0, 0, 0, 0
 p_modifiers = [221, 212, 211, 202, 201, 192, 191, 182, 181, 172]
 s_modifiers = {
   nil => 0.5,
@@ -26,10 +24,10 @@ skills_modifiers = {
 }
 
 (p_modifiers + [0]).reverse.each do |p_modifier|
+  modifiers_sum = 0
+  pct10_sum, pct25_sum, pct50_sum, pct75_sum, pct90_sum = 0, 0, 0, 0, 0
   CSV.foreach('salaries.csv', :headers => true) do |row|
-    if row["pct10"] == "0" && row["pct25"] == "0" && row["pct50"] == "0" && row["pct75"] == "0" && row["pct90"] == "0" || row["p"] != p_modifier.to_s
-      # puts "lol"
-    else
+    unless row["pct10"] == "0" && row["pct25"] == "0" && row["pct50"] == "0" && row["pct75"] == "0" && row["pct90"] == "0" || row["p"] != p_modifier.to_s
       modifier = 1.0
       if row["p"] != "0"
         modifier /= 2 ** p_modifiers.find_index(row["p"].to_i)
@@ -49,11 +47,6 @@ skills_modifiers = {
       pct50_sum += row["pct50"].to_i * modifier
       pct75_sum += row["pct75"].to_i * modifier
       pct90_sum += row["pct90"].to_i * modifier
-      # print([(pct10_sum / modifiers_sum).round,
-      #        (pct25_sum / modifiers_sum).round,
-      #        (pct50_sum / modifiers_sum).round,
-      #        (pct75_sum / modifiers_sum).round,
-      #        (pct90_sum / modifiers_sum).round], "\n")
     end
   end
 
@@ -63,3 +56,36 @@ skills_modifiers = {
          (pct75_sum / modifiers_sum).round,
          (pct90_sum / modifiers_sum).round], "\n")
 end
+
+modifiers_sum = 0
+pct10_sum, pct25_sum, pct50_sum, pct75_sum, pct90_sum = 0, 0, 0, 0, 0
+
+CSV.foreach('salaries.csv', :headers => true) do |row|
+  unless row["pct10"] == "0" && row["pct25"] == "0" && row["pct50"] == "0" && row["pct75"] == "0" && row["pct90"] == "0"
+    modifier = 1.0
+    if row["p"] != "0"
+      modifier /= 2 ** p_modifiers.find_index(row["p"].to_i)
+    else
+      modifier /= 2 ** (p_modifiers.length / 2.0)
+    end
+    modifier *= s_modifiers[row["s"]]
+    modifier *= q_modifiers[row["q"]]
+    modifier *= city_modifiers[row["city"]]
+    modifier *= (0.5 +
+      skills_modifiers[row["skill1"]].to_i +
+      skills_modifiers[row["skill2"]].to_i +
+      skills_modifiers[row["skill3"]].to_i)
+    modifiers_sum += modifier
+    pct10_sum += row["pct10"].to_i * modifier
+    pct25_sum += row["pct25"].to_i * modifier
+    pct50_sum += row["pct50"].to_i * modifier
+    pct75_sum += row["pct75"].to_i * modifier
+    pct90_sum += row["pct90"].to_i * modifier
+  end
+end
+
+print(["current", ": ", (pct10_sum / modifiers_sum).round,
+       (pct25_sum / modifiers_sum).round,
+       (pct50_sum / modifiers_sum).round,
+       (pct75_sum / modifiers_sum).round,
+       (pct90_sum / modifiers_sum).round], "\n")
